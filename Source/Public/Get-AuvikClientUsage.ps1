d07c9509feace8ee24607363eb13863071fea589
Mike Hiersche <hierschemichael@gmail.com>
2025-06-30 15:15:08 -0400

    Add Get-AuvikClientUsage

---
+++ Source/Public/Get-AuvikClientUsage.ps1
@@ -0,0 +1,54 @@
+function Get-AuvikClientUsage {
+    [CmdletBinding()]
+    param (
+        # Filter by alert definition ID.
+        [Parameter(Mandatory)]
+        [datetime]
+        $FromDate,
+        # Deprecated. Filter by alert specification ID. Use 'alertDefinitionId' instead
+        [Parameter(Mandatory)]
+        [datetime]
+        $ThruDate,
+        # Array of tenant IDs to request info from.
+        [Parameter()]
+        [string[]]
+        $TenantID,
+        # Maximum number of pages of results to get
+        [Parameter()]
+        [int]
+        $Pages
+    )
+
+    begin {
+        if (-not($ConnectedToAuvik)) {
+            Throw "Authentication needed. Please call Connect-AuvikApi"
+        }
+
+        $QueryParams = foreach ($Key in $PSBoundParameters.Keys) {
+            switch ($Key) {
+                FromDate {
+                    "filter[fromDate]=$(Get-Date -Date $FromDate -Format "yyyy-MM-dd")"
+                }
+                ThruDate {
+                    "filter[thruDate]=$(Get-Date -Date $ThruDate -Format "yyyy-MM-dd")"
+                }
+                TenantID {
+                    "tenants=$($TenantID -join ",")"
+                }
+                default {}
+            }
+        }
+
+        $Parameters = if ($Pages) {
+            @{'Pages' = $Pages}
+        }
+    }
+
+    process {
+        $AuvikClientUsage = (Invoke-AuvikApi -Uri "$($AuvikBaseUri)/https://auvikapi.us3.my.auvik.com/v1/billing/usage/client?$($QueryParams -join '&')" @Parameters).data | ForEach-Object {[AuvikClientUsage]::new($_)}
+    }
+
+    end {
+        return $AuvikClientUsage
+    }
+}
